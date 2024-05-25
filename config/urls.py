1ef8ce98e7c78d5db04a2ebab047ae8069b477be
mobin-gpr <mohammad.mobin.ghanbarpour79@gmail.com>
2024-05-25 23:48:43 +0330

    add api-auth url

--- config/urls.py
+++ config/urls.py
@@ -23,6 +23,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path("ckeditor5/", include('django_ckeditor_5.urls')),
     path('articles/api/v1/', include('articles_app.api.v1.urls')),
+    path('api-auth/', include('rest_framework.urls'))
 ]
 
 urlpatterns += urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
