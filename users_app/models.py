61ecad64de8033d12c6815272c29fc7407021daa
mobin-gpr <mohammad.mobin.ghanbarpour79@gmail.com>
2024-06-25 01:12:12 +0330

    debug user model

--- users_app/models.py
+++ users_app/models.py
@@ -31,6 +31,7 @@ class UserManager(BaseUserManager):
         extra_fields.setdefault("is_staff", True)
         extra_fields.setdefault("is_superuser", True)
         extra_fields.setdefault("is_verified", True)
+        extra_fields.setdefault("is_active", True)
 
         if extra_fields.get("is_staff") is not True:
             raise ValueError("Superuser must have is_staff=True.")
